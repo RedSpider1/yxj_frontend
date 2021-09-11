387a95c48c63262d694e47efaadc4a220bf2bfd9
coolerwu <1447186835@qq.com>
2021-09-11 21:50:38 +0800

    202109011 fix: cloudId => cloudID

--- utils/auth.js
+++ utils/auth.js
@@ -7,17 +7,24 @@ function login () {
   const app = getApp()
   const pages = getCurrentPages()
   const sourceRoute = pages[pages.length - 1].route
-  wx.login({
-    success: res => {
-      app.globalData.code = res.code
-      http.post(request.login.url, {jsCode: res.code}).then(res => {
-        app.globalData.authToken = res.token
-        wx.navigateTo({
-          url: `/pages/me/login/index?sourceRoute=${sourceRoute}`,
+  // 有无token来进行处理
+  if (string.isEmpty(app.globalData.authToken)) {
+    wx.login({
+      success: res => {
+        app.globalData.code = res.code
+        http.post(request.login.url, {jsCode: res.code}).then(res => {
+          app.globalData.authToken = res.token
+          wx.navigateTo({
+            url: `/pages/me/login/index?sourceRoute=${sourceRoute}`,
+          })
         })
-      })
-    }
-  })
+      }
+    })
+  } else {
+    wx.navigateTo({
+      url: `/pages/me/login/index?sourceRoute=${sourceRoute}`,
+    })
+  }
 }
 
 export const checkLogin = function () {
