b068469e70aad6b65d1951c64e143b5a6db3f076
coolerwu <1447186835@qq.com>
2021-09-11 17:07:15 +0800

    202109011 fix: 恢复组队单详情

--- pages/me/login/index.js
+++ pages/me/login/index.js
@@ -41,20 +41,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
